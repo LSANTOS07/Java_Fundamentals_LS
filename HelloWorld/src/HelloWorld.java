da783d63831bfef74fde509f17954b67050f78df
Joao Brito <joaobrito.eng@gmail.com>
2014-06-24 21:42:55 +0100

    first commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Training Center
+ *
+ */
+public class HelloWorld {
+	
+	
+/** 
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Olá Mundo!");
+	}
+
+}
+
+
